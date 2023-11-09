e79103ac10a90ccf4bff805a52aaf1796eb9fadb
Ben-Lassina <105428781+ben-Lassina@users.noreply.github.com>
2023-11-09 22:33:35 +0100

    Update navbar.php

--- public/navbar.php
+++ public/navbar.php
@@ -1,6 +1,6 @@
 <header class="header">
     <nav class="navbar">
-    <a href="index.php"><img src="img/logo.webp"/></a>
+        <a href="index.php"><img src="img/logo.webp"/></a>
         <ul class="menu">
             <li><a href="#content">Over ons</a></li>
             <li><a href="#footer">Contact</a></li>
